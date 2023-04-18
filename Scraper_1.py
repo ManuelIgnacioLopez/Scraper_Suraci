aa901a13c803887170e6e1c937d24e51b1acd043
Gabriel Pérez Diez <31172061+Gabsplat@users.noreply.github.com>
2023-04-18 13:20:15 -0300

    Update Scraper_1.py

--- Scraper_1.py
+++ Scraper_1.py
@@ -8,7 +8,6 @@ from selenium.common.exceptions import NoSuchElementException
 import pandas as pd
 import numpy as np
 import time
-import csv
 import os
 import gspread
 from pyvirtualdisplay import Display
