792cdb741f2063770b26c9c65591b6902121f736
ManuelIgnacioLopez <103151064+ManuelIgnacioLopez@users.noreply.github.com>
2023-05-04 17:12:00 -0300

    Update scrapper_2.py

--- scrapper_2.py
+++ scrapper_2.py
@@ -38,6 +38,6 @@ driver.get(urls_inmoclick[0])
 time.sleep(5)
 largoUrl=driver.find_element(By.XPATH,'/html/body/nav/form/div[2]/div[2]/div/div/div[1]/h2').text
 largoUrl=largoUrl.split()
-largoUrl=int(largoUrl[0])
+largoUrl=int(largoUrl)
 largoUrl=int(largoUrl/48)
 
