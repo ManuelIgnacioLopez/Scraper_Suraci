878ca3d769d278156b006c296991c30697d358d0
Gabriel Pérez Diez <31172061+Gabsplat@users.noreply.github.com>
2023-05-04 21:14:28 -0300

    Actualizar scrapper_2.py

--- scrapper_2.py
+++ scrapper_2.py
@@ -19,7 +19,9 @@ display.start()
 
 #Inicio
 
-driver = uc.Chrome(use_subprocess=True)
+
+driver = uc.Chrome(version_main = 112)
+#driver = uc.Chrome(use_subprocess=True)
 
 urls_inmoclick=[
     'https://www.inmoclick.com.ar/locales-comerciales-en-alquiler?favoritos=0&limit=48&prevEstadoMap=&localidades=1%2C2%2C8%2C19%2C7%2C6%2C10&lastZoom=13&precio%5Bmin%5D=&precio%5Bmax%5D=&moneda=1&sup_cubierta%5Bmin%5D=&sup_cubierta%5Bmax%5D=&expensas%5Bmin%5D=&expensas%5Bmax%5D=',
