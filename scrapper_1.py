3c01897c0afedf5bf3cf3fb0c370534196f196aa
ManuelIgnacioLopez <103151064+ManuelIgnacioLopez@users.noreply.github.com>
2023-05-05 14:38:58 -0300

    Update scrapper_1.py

--- scrapper_1.py
+++ scrapper_1.py
@@ -75,7 +75,7 @@ cate = cate[::2]
 dolarz=False    
 while dolarz==False:
   try:
-    driver.get('https://www.cronista.com/MercadosOnline/moneda.html?id=ARSMEP')
+    driver.get('https://www.cronista.com/MercadosOnline/moneda.html?id=ARSB')
     ele = driver.find_element(By.XPATH,'//*[@id="market-scrll-1"]/tbody/tr/td[2]/a/div/div[2]')
     dollar=ele.text
     dollar= dollar.replace('$', '')
